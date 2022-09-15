b484a3b57681f3320b7a750399cb73ff63371812
agustinaa235 <asegura@fi.uba.ar>
2022-09-15 16:11:05 -0300

    Fix: routes

--- server.js
+++ server.js
@@ -1,11 +1,34 @@
-const express = require('express');
+const routes = require('./routes/routes.js')
+const bodyParser = require('body-parser')
+const path = require('path')
+const express = require('express')
+const swaggerUi = require('swagger-ui-express');
+const swaggerJSDoc = require('swagger-jsdoc');
+const swaggerSpec = {
+    definition : {
+        openapi: "3.0.0",
+        info: {
+            title: "API-Journey",
+            version: "1.0.0",
+        },
+        servers: [
+            {
+                url: "http://localhost:9000"
+            }
+        ]
+    },
+    apis: [`${__dirname}/routes/routes.js`],  
+};
 
 
+const app = express()
 
-const app = express();
 
+app.use(bodyParser.json());
+app.use('/docs', swaggerUi.serve, swaggerUi.setup(swaggerJSDoc(swaggerSpec)));
+app.use("/journeys",routes.journeyRouter);
 
-app.get('/', (req, res) => {
+app.get('/example', (req, res) => {
    res.send('Hello World');
 });
 
