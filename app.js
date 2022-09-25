de0e5782e47475e76179725204701583594dffdf
solfonte <msfontenla@fi.uba.ar>
2022-09-25 16:32:44 -0300

    feat:added app config file

--- app.js
+++ app.js
@@ -1,6 +1,30 @@
 const express = require('express');
 const app = express();
+const routes = require('./routes/routes.js')
+const bodyParser = require('body-parser')
+const path = require('path')
+const swaggerUi = require('swagger-ui-express');
+const swaggerJSDoc = require('swagger-jsdoc');
+const swaggerSpec = {
+    definition : {
+        openapi: "3.0.0",
+        info: {
+            title: "API-Journey",
+            version: "1.0.0",
+        },
+        servers: [
+            {
+                url: "http://localhost:9000"
+            }
+        ]
+    },
+    apis: [`${__dirname}/routes/routes.js`],  
+};
 
+
+app.use(bodyParser.json());
+app.use('/docs', swaggerUi.serve, swaggerUi.setup(swaggerJSDoc(swaggerSpec)));
+app.use("/journeys",routes.journeyRouter);
 
 app.get('/', (req, res) => {
     res.send('Journey service!');
