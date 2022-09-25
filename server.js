132e4b6990b339f1830009d776bdc2a862350a19
solfonte <msfontenla@fi.uba.ar>
2022-09-25 16:19:00 -0300

    feat: docker setup

--- server.js
+++ server.js
@@ -1,13 +1,4 @@
-const express = require('express');
-
-
-
-const app = express();
-
-
-app.get('/', (req, res) => {
-   res.send('Hello World');
-});
+const {app} = require('./app')
 
 const PORT = process.env.PORT || 9000;
 app.listen(PORT, () => console.log(`Server running at port ${PORT}`));
