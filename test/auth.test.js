7f08e4711f532d0226a19610d8b954ad5aa54112
alejovillores <avillores@fi.uba.ar>
2022-10-04 18:06:06 -0300

    test: Invalid token value

--- test/auth.test.js
+++ test/auth.test.js
@@ -11,9 +11,22 @@ describe('Auth', () => {
     expect(auth.validate(header)).toBe('token');
   } );
 
-  test('02_should_throw_error_token', () => {
+  test('02_should_throw_error_token_invalid_header', () => {
     header = {
       'Bla bla': 'Bearer token',
+    };
+
+    const auth = new Auth();
+    const error = () => {
+      auth.validate(header);
+    };
+
+    expect(error).toThrow(InvalidTokenError);
+  } );
+
+  test('03_should_throw_error_token_not_bearer', () => {
+    header = {
+      'Authorization': 'NotBearer token',
     };
 
     const auth = new Auth();
