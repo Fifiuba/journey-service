58f45c221be2e836beeec0d83cccb4e0901e4a0e
solfonte <msfontenla@fi.uba.ar>
2022-10-12 15:56:40 -0300

    refactor: changed return value of getJourneyById

--- model/journeyRepository.js
+++ model/journeyRepository.js
@@ -1,11 +1,11 @@
 const {JourneyModel} = require("../database/database")
 
+//returns null if the journey is not found
 async function getJourneyById(id){
     var journey = await JourneyModel.findById(id, function (err){
       return err;
     }).clone().catch(function(err){ console.log(err)});
-    
-    if (!journey) return {error: "journey not found"};
+  
     return journey;
     
   }
