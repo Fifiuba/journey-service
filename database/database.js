b3ca9b7bf13ffb65b29476b42a8a9e459331f696
solfonte <msfontenla@fi.uba.ar>
2022-11-06 09:09:53 -0300

    fix: added try catch block to connect to db

--- database/database.js
+++ database/database.js
@@ -1,20 +1,28 @@
 const mongoose = require('mongoose');
 
+function sleep(n){
+  return new Promise(resolve => setTimeout(resolve,n));
+}
+
+
 class JourneyDatabase {
   async connectDB() {
-    await mongoose.connect('mongodb://root:root@mongodb:27017', {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-      auth: {
-        authSource: 'admin',
-      },
-      user: process.env.ME_CONFIG_MONGODB_ADMINUSERNAME,
-      pass: process.env.ME_CONFIG_MONGODB_ADMINPASSWORD,
-      dbName: 'Journey_db',
+    try{
+      await mongoose.connect('mongodb://root:root@mongodb:27017', {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+        auth: {
+          authSource: 'admin',
+        },
+        user: process.env.ME_CONFIG_MONGODB_ADMINUSERNAME,
+        pass: process.env.ME_CONFIG_MONGODB_ADMINPASSWORD,
+        dbName: 'Journey_db',
 
-    }).then(() => {
-      console.log('Mongodb connected...');
-    });
+      })
+        console.log('Mongodb connected...');
+    }catch (error){
+      console.log();
+    }
   }
 
 
