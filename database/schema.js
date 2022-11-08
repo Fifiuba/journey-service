eac685b8f72c0fbb9a8a8c6dabc20399dd0cdbb9
solfonte <msfontenla@fi.uba.ar>
2022-11-08 15:18:01 -0300

    feat: added configuration colection to db

--- database/schema.js
+++ database/schema.js
@@ -1,9 +1,10 @@
+const { Decimal128 } = require('mongodb');
 const mongoose = require('mongoose');
 const {Schema} = mongoose;
 
 // Each schema maps to a MongoDB collection and defines
 // the shape of the documents within that collection
-const JourneySchema = new Schema({
+const JourneySchema = new Schema ({
   status: String,
   idPassenger: Number,
   driver: {
@@ -17,9 +18,15 @@ const JourneySchema = new Schema({
   to: {type: [Number]},
 });
 
+const ConfigurationSchema = new Schema ({
+  price: Number,
+  radial_distance: Decimal128,
+  unit: String,
+});
+
 // To use our schema definition, we need to convert it
 // into a Model we can work with Instances of Models are documents
 const JourneyModel = mongoose.model('Journey', JourneySchema);
+const ConfigurationModel = mongoose.model('Configuration', ConfigurationSchema);
 
-
-module.exports = {JourneyModel};
+module.exports = {JourneyModel, ConfigurationModel};
